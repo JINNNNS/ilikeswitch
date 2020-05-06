b31b1d0bd0e9b52ff7bddd06d81d7cdee7b495e5
JINNNNS <baekhuen@naver.com>
2020-05-06 18:10:50 +0900

    초기 커밋.

---
+++ ilikeswitch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilikeswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("학년을 입력하세요: ");
+            int level = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
